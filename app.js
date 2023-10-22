a38f995038e476411e64e26af6840c248717b2d9
21127615 <bghuy2003@gmail.com>
2023-10-22 16:43:20 +0700

    check validate

--- app.js
+++ app.js
@@ -92,3 +92,208 @@ newsData.forEach((data) => {
     newsContainer.appendChild(newsElement);
 });
 createMenuElement(headerMenuContainer);
+
+const infoContainer = document.querySelectorAll('.answer');
+
+// Biến để lưu trữ thông báo lỗi
+let errorNamerDiv = null;
+function validateNameInput() {
+
+    const inputName = document.getElementById('input-name');
+    const nameInputContainer = document.querySelector('.name-input');
+    const nameValue = inputName.value.trim();
+    const nameWords = nameValue.split(' ').filter(word => word !== '');
+    let nameError = false;
+    // Xóa thông báo lỗi hiện tại nếu có
+    if (errorNamerDiv) {
+        errorNamerDiv.remove();
+        errorNamerDiv = null;
+    }
+    if (nameWords.length < 2) {
+        errorNamerDiv = document.createElement('div');
+        errorNamerDiv.textContent = 'Họ và tên chưa hợp lệ';
+        errorNamerDiv.style.color = 'red';
+        errorNamerDiv.style.width = '100%';
+        errorNamerDiv.style.textIndent = '48%';
+        nameInputContainer.insertAdjacentElement('afterend', errorNamerDiv);
+        inputName.style.border = '2px solid red';
+        nameError = true;
+    }
+    else {
+        nameError = false;
+        inputName.style.border = '';
+    }
+}
+let errorPhoneDiv = null;
+function validatePhoneNumberInput() {
+
+    const inputPhone = document.getElementById('input-phoneNumber');
+    const phoneInputContainer = document.querySelector('.phone-input');
+    const phoneValue = inputPhone.value.trim();
+    const phoneRegex = /^0[0-9]{9}$/; // Regex để kiểm tra số điện thoại
+
+    // Xóa thông báo lỗi hiện tại nếu có
+    if (errorPhoneDiv) {
+        errorPhoneDiv.remove();
+        errorPhoneDiv = null;
+    }
+
+    if (!phoneRegex.test(phoneValue)) {
+        errorPhoneDiv = document.createElement('div');
+        errorPhoneDiv.textContent = 'Số điện thoại chưa hợp lệ';
+        errorPhoneDiv.style.color = 'red';
+        errorPhoneDiv.style.width = '100%';
+        errorPhoneDiv.style.textIndent = '48%';
+        phoneInputContainer.insertAdjacentElement('afterend', errorPhoneDiv);
+        inputPhone.style.border = '2px solid red';
+        return false;
+    } else {
+        inputPhone.style.border = '';
+    }
+    return true;
+}
+let errorEmailDiv = null;
+function validateEmailInput() {
+
+    const inputEmail = document.getElementById('input-email');
+    const emailInputContainer = document.querySelector('.email-input');
+    const emailValue = inputEmail.value.trim();
+
+    // Xóa thông báo lỗi hiện tại nếu có
+    if (errorEmailDiv) {
+        errorEmailDiv.remove();
+        errorEmailDiv = null;
+    }
+
+    if (!emailValue.endsWith('@gmail.com')) {
+        errorEmailDiv = document.createElement('div');
+        errorEmailDiv.textContent = 'Email chưa hợp lệ';
+        errorEmailDiv.style.color = 'red';
+        errorEmailDiv.style.width = '100%';
+        errorEmailDiv.style.textIndent = '48%';
+        emailInputContainer.insertAdjacentElement('afterend', errorEmailDiv);
+        inputEmail.style.border = '2px solid red';
+    } else {
+        inputEmail.style.border = '';
+    }
+}
+
+let errorDeliveryDayInput = null;
+
+function validateDeliveryDay() {
+    const deliveryDayInput = document.getElementById('input-deliveryDay');
+    const deliveryDayContainer = document.querySelector('.deliveryDay-input');
+    const deliveryDayValue = deliveryDayInput.value;
+
+    // Lấy ngày hôm nay
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    // Chuyển đổi ngày giao hàng thành đối tượng Date
+    const deliveryDay = new Date(deliveryDayValue);
+
+    // Xóa thông báo lỗi nếu có
+    if (errorDeliveryDayInput) {
+        errorDeliveryDayInput.remove();
+        errorDeliveryDayInput = null;
+    }
+
+    // Kiểm tra nếu ngày giao hàng sau ngày hôm nay
+    if (deliveryDay > today) {
+        // Ngày giao hợp lệ
+        deliveryDayInput.style.border = '';
+    } else {
+        // Ngày giao không hợp lệ
+        errorDeliveryDayInput = document.createElement('div');
+        errorDeliveryDayInput.textContent = 'Ngày giao hàng không hợp lệ.';
+        errorDeliveryDayInput.style.color = 'red';
+        errorDeliveryDayInput.style.width = '100%';
+        errorDeliveryDayInput.style.textIndent = '48%';
+        deliveryDayContainer.insertAdjacentElement('afterend', errorDeliveryDayInput);
+        deliveryDayInput.style.border = '2px solid red';
+    }
+}
+
+function validateGender() {
+    const genderInputs = document.getElementsByName('gender');
+    let selectedGender = null;
+
+    // Lặp qua tất cả các phần tử input có name="gender"
+    for (let i = 0; i < genderInputs.length; i++) {
+        if (genderInputs[i].checked) {
+            if (selectedGender === null) {
+                // Chưa có giới tính được chọn trước đó
+                selectedGender = genderInputs[i].value;
+            } else {
+                // Đã có một giới tính được chọn trước đó
+                selectedGender = null; // Đặt giá trị chọn giới tính là null để đánh dấu là đã chọn cả hai
+                break; // Dừng vòng lặp nếu đã tìm thấy cả hai giới tính được chọn
+            }
+        }
+    }
+
+    // Kiểm tra kết quả chọn giới tính
+    if (selectedGender === null) {
+        // Người dùng không chọn giới tính hoặc đã chọn cả hai giới tính
+        const errorDiv = document.getElementById('gender-error');
+        if (!errorDiv) {
+            // Tạo thông báo lỗi nếu chưa tồn tại
+            const errorDiv = document.createElement('div');
+            errorDiv.id = 'gender-error';
+            errorDiv.textContent = 'yêu cầu chọn giới tính';
+            errorDiv.style.color = 'red';
+            errorDiv.style.width = '100%';
+            errorDiv.style.textIndent = '48%';
+            const genderContainer = document.querySelector('.gender-input');
+
+            genderContainer.insertAdjacentElement('afterend', errorDiv);
+        }
+    } else {
+        // Người dùng đã chọn giới tính hợp lệ
+        const errorDiv = document.getElementById('gender-error');
+        if (errorDiv) {
+            errorDiv.remove();
+            errorDiv = null;
+        }
+    }
+}
+function validateAddress() {
+    const addressInput = document.getElementById('input-address');
+    const addressValue = addressInput.value.trim();
+
+    if (addressValue === '') {
+        // Người dùng không nhập địa chỉ
+        const errorDiv = document.getElementById('address-error');
+        if (!errorDiv) {
+            // Tạo thông báo lỗi nếu chưa tồn tại
+            const errorDiv = document.createElement('div');
+            errorDiv.id = 'address-error';
+            errorDiv.textContent = 'Vui lòng nhập địa chỉ.';
+            errorDiv.style.color = 'red';
+            errorDiv.style.width = '100%';
+            errorDiv.style.textIndent = '48%';
+            const addressContainer = document.querySelector('.address-input');
+
+            addressInput.style.border = '2px solid red';
+            addressContainer.insertAdjacentElement('afterend', errorDiv);
+        }
+    } else {
+        // Người dùng đã nhập địa chỉ
+        const errorDiv = document.getElementById('address-error');
+        if (errorDiv) {
+            // Xóa thông báo lỗi nếu tồn tại
+            addressInput.style.border = '';
+            errorDiv.remove();
+        }
+    }
+}
+function validateFormInput() {
+    validateNameInput();
+    validatePhoneNumberInput();
+    validateEmailInput();
+    validateDeliveryDay();
+    validateGender();
+    validateAddress();
+}
+const submitBtn = document.querySelector('.submitBtn');
+submitBtn.addEventListener('click', validateFormInput);
